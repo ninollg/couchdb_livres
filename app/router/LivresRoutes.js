2a73dcb2d2f97cfb681845ca32254a18a2256bc3
ninollg <nino.leliege@gmail.Com>
2023-12-12 20:12:20 +0100

    mvc routes

--- app/router/LivresRoutes.js
+++ app/router/LivresRoutes.js
@@ -4,6 +4,13 @@ const LivresRouter = express.Router();
 const LivresController = require("../controller/LivresController.js");
 
 LivresRouter.get("/livres", LivresController.liste);
+LivresRouter.get("/livres/:idLivre", LivresController.afficherLivre);
+LivresRouter.get("/livres/:idLivre/pages", LivresController.afficherPages);
+LivresRouter.get("/livres/:idLivre/pages/:numPage", LivresController.afficherPage);
+LivresRouter.post("/livres", LivresController.ajouterLivre);
+LivresRouter.delete("/livres/:numLivre", LivresController.supprimerLivre);
+LivresRouter.put("/livres", LivresController.modifierLivre);
+
 
 
 module.exports={LivresRouter}
